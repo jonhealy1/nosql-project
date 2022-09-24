cae68873fc0f8d74c9a12733356cdbc21a6a4e9c
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-24 12:00:10 +0300

    add simple dal

---
+++ dal1_1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type dal struct {
+	file *os.File
+}
+
+func newDal(path string) (*dal, error) {
+	dal := &dal{}
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, err
+	}
+	dal.file = file
+	return dal, nil
+}
+
+func (d *dal) close() error {
+	if d.file != nil {
+		err := d.file.Close()
+		if err != nil {
+			return fmt.Errorf("could not close file: %s", err)
+		}
+		d.file = nil
+	}
+	return nil
+}
