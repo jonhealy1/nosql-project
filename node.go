8f2be031b822efb770619b266ea91bdf8b66e84c
jonhealy1 <jonathan.d.healy@gmail.com>
2023-02-13 18:49:39 +0300

    read/write node

--- node.go
+++ node.go
@@ -142,3 +142,36 @@ func (n *Node) deserialize(buf []byte) {
 		n.childNodes = append(n.childNodes, pageNum)
 	}
 }
+
+func (d *dal) getNode(pageNum pgnum) (*Node, error) {
+	p, err := d.readPage(pageNum)
+	if err != nil {
+		return nil, err
+	}
+	node := NewEmptyNode()
+	node.deserialize(p.data)
+	node.pageNum = pageNum
+	return node, nil
+}
+
+func (d *dal) writeNode(n *Node) (*Node, error) {
+	p := d.allocateEmptyPage()
+	if n.pageNum == 0 {
+		p.num = d.getNextPage()
+		n.pageNum = p.num
+	} else {
+		p.num = n.pageNum
+	}
+
+	p.data = n.serialize(p.data)
+
+	err := d.writePage(p)
+	if err != nil {
+		return nil, err
+	}
+	return n, nil
+}
+
+func (d *dal) deleteNode(pageNum pgnum) {
+	d.releasePage(pageNum)
+}
